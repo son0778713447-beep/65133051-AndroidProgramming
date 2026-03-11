479256892278be569dd92bfb5e619bf156242e1a
Nguyễn Trường Sơn <son.0778713447@gmail.com>
2026-03-11 20:57:45 +0700

    TH2_2

--- TH2/app/src/main/java/nts/cntt2/th2/MainActivity.java
+++ TH2/app/src/main/java/nts/cntt2/th2/MainActivity.java
@@ -1,6 +1,8 @@
 package nts.cntt2.th2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    //Đây là bộ lắng nghe và xử lý sự kiện click lên nút Tính Tổng
+    public void XuLyCong(View view){
+        // Tìm tham chiếu đến điều khiển trên tập XML
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKQ);
+        //Lấy dữ liệu về ở điều khiển số a
+        String strA = editTextSoA.getText().toString();
+        //Lấy dữ liệu về ở điều khiển số b
+        String strB = editTextSoB.getText().toString();
+        //Chuyển dữ liệu sang số
+        int soA = Integer.parseInt(strA);
+        int soB = Integer.parseInt(strB);
+        //Tính toán
+        int kq = soA+soB;
+        String strTong = String.valueOf(kq);
+        //Xuất kết quả ra màn hình
+        editTextKQ.setText(strTong);
     }
 }
