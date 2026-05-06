2e8a5b09a0d4410dfaa5750095cfdba974840906
Nguyễn Trường Sơn <son.0778713447@gmail.com>
2026-05-06 14:50:22 +0700

    ViecCanLam phần 1+2

---
+++ ViecCanLam/app/src/main/java/nts/cntt2/vieccanlam/MainActivity.java
@@ -0,0 +1,57 @@
+package nts.cntt2.vieccanlam;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<TASKS> lstVCL;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        // Write a message to database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("TASKS");
+        //Lang nghe
+        lstVCL = new ArrayList<TASKS>();
+        myRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Lay du lieu
+                for (DataSnapshot obj:snapshot.getChildren()){
+                    TASKS task =  obj.getValue(TASKS.class);
+                    lstVCL.add(task);
+                    Log.w("VCL app","Việc cần làm: "+task.getName());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
---
+++ ViecCanLam/app/src/main/java/nts/cntt2/vieccanlam/TASKS.java
@@ -0,0 +1,59 @@
+package nts.cntt2.vieccanlam;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+public class TASKS implements Serializable {
+    String name,date,message,priotiry;
+
+    public TASKS(String name, String date, String message, String priotiry) {
+        this.name = name;
+        this.date = date;
+        this.message = message;
+        this.priotiry = priotiry;
+    }
+
+    public TASKS() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPriotiry() {
+        return priotiry;
+    }
+
+    public void setPriotiry(String priotiry) {
+        this.priotiry = priotiry;
+    }
+    //
+    public HashMap<String,String> toFirebaseObject(){
+        HashMap<String,String> taskObject = new HashMap<String, String>();
+        taskObject.put("name",name);
+        taskObject.put("date",date);
+        taskObject.put("message",message);
+        taskObject.put("priority",priotiry);
+        return taskObject;
+    }
+}
